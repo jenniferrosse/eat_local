190261e6314cfb2e7efc0ee25fa532f4660a6736
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-08-08 07:14:18 -0500

    Initial Commit

---
+++ db/migrate/20160801231404_create_restaurants.rb
@@ -0,0 +1,13 @@
+class CreateRestaurants < ActiveRecord::Migration
+  def change
+    create_table :restaurants do |t|
+      t.string :name
+      t.string :url
+      t.string :address
+      t.float :latitude
+      t.float :longitude
+
+      t.timestamps null: false
+    end
+  end
+end
