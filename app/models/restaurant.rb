f0d493b12ce1ebf206b9988573ff18b97eaed609
Jennifer Rosse <jenniferrosse26@gmail.com>
2016-08-09 14:36:10 -0500

    Added validation to restaurants model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ActiveRecord::Base
+  validates :name, :address, :url, :presence => true
+  validates :address, :uniqueness => true
 
   geocoded_by :address
   after_validation :geocode, :if => :address_changed?
